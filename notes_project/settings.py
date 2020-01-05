e99201c8f076d33ac3fac7461dee483650b80a49
Dheeraj Kumar <dkchalotra@gmail.com>
2020-01-05 17:42:27 +0530

    Bug fix

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
