77fd046d3666a7dbb0a1fd74edf244f9e846f7f3
Dheeraj Kumar <dkchalotra@gmail.com>
2020-01-10 18:03:55 +0530

    Meta Tag viewport fix

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'd%!w1#djqg&=j8=*r*2r2&u7%zu#n!rwt9qvo=wymwey0t&nhx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stickynotesapp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stickynotesapp.herokuapp.com', '192.168.31.155', ]
 
 # Application definition
 
